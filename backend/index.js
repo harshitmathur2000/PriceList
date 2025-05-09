17282782e026fbdf076ed8f1cd62280f60233efa
harshitmathur2000 <mathurharshit16nov@gmail.com>
2025-05-09 17:34:52 +0530

    log fix

--- backend/index.js
+++ backend/index.js
@@ -109,7 +109,7 @@ const start = async () => {
     await sequelize.authenticate();
     await sequelize.sync();
     await fastify.listen({ port: PORT, host: '0.0.0.0' });
-    console.log('Server running on http://localhost:' + PORT);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
   } catch (err) {
     console.error(err);
   }
