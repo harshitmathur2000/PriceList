1f2f86142a96da1a764c78dc99228667a49bfaf5
harshitmathur2000 <mathurharshit16nov@gmail.com>
2025-05-09 18:38:52 +0530

    prev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   const [addProductForm, setAddProductForm] = useState(false);
   const fetchProducts = async () => {
     const res = await axios.get(API);
-    console.log(red.data);
+    console.log(res.data);
     setProducts(res.data);
   };
 
